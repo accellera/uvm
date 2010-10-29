d665922f8f9e199ae25f182e0b80204e89e69df3
Uwe Simm <uwes@cadence.com>
2010-10-29 14:43:23 +0200

    better integrated with test structure

--- tests/14ovm2uvm/test.sh
+++ tests/14ovm2uvm/test.sh
@@ -26,4 +26,5 @@ cp -fr ovm_sources  some_ve
 
 ../../distrib/bin/ovm2uvm.pl --top_dir ./some_ve --marker "XX-REVIEW-XX" --write --backup --all_text_files
 
-diff some_ve uvm_sources.golden > test.diff
+diff some_ve uvm_sources.golden > /dev/null
+
