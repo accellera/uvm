b8e5d7e6e97ec6e61db0389cf42d2d679ea3f8c4
Uwe Simm <uwes@cadence.com>
2010-10-29 15:35:20 +0200

    updated test to align with inca/api changes

--- tests/70regs/30bkdr/10paths/test.sv
+++ tests/70regs/30bkdr/10paths/test.sv
@@ -20,128 +20,153 @@
 
 program top;
 
-import uvm_pkg::*;
+    import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-class r1_typ extends uvm_reg;
+    class r1_typ extends uvm_reg;
 
-   function new(string name = "r1_typ");
-      super.new(name,32,UVM_NO_COVERAGE);
-   endfunction
+        function new(string name = "r1_typ");
+            super.new(name,32,UVM_NO_COVERAGE);
+        endfunction
 
-   virtual function void build();
-   endfunction
+        virtual function void build();
+        endfunction
    
-   `uvm_object_utils(r1_typ)
+        `uvm_object_utils(r1_typ)
    
-endclass
+    endclass
 
 
-class b1_typ extends uvm_reg_block;
+    class b1_typ extends uvm_reg_block;
 
-   rand r1_typ r1; 
+        rand r1_typ r1; 
 
-   function new(string name = "b1_typ");
-      super.new(name,UVM_NO_COVERAGE);
-   endfunction
+        function new(string name = "b1_typ");
+            super.new(name,UVM_NO_COVERAGE);
+        endfunction
 
-   virtual function void build();
+        virtual function void build();
 
-      r1 = r1_typ::type_id::create("r1");
-      r1.build();
-      r1.configure(this,null,"r1");
-   endfunction
+            r1 = r1_typ::type_id::create("r1");
+            r1.build();
+            r1.configure(this,null,"r1");
+        endfunction
    
-   `uvm_object_utils(b1_typ)
+        `uvm_object_utils(b1_typ)
    
-endclass
+    endclass
 
 
-class top_blk extends uvm_reg_block;
+    class top_blk extends uvm_reg_block;
 
-   rand b1_typ b1; 
+        rand b1_typ b1; 
 
-   function new(string name = "top_blk");
-      super.new(name,UVM_NO_COVERAGE);
-   endfunction
+        function new(string name = "top_blk");
+            super.new(name,UVM_NO_COVERAGE);
+        endfunction
 
-   virtual function void build();
+        virtual function void build();
 
-      b1 = b1_typ::type_id::create("b1");
-      b1.build();
-      b1.configure(this,"b1");
-   endfunction
+            b1 = b1_typ::type_id::create("b1");
+            b1.build();
+            b1.configure(this,"b1");
+        endfunction
    
-   `uvm_object_utils(top_blk)
+        `uvm_object_utils(top_blk)
    
-endclass
+    endclass
 
 
-function void check_roots(string name,
-                          string roots[$],
-                          string exp[]);
-   $write("Path(s) to %s:\n", name);
-   foreach (roots[i]) begin
-      $write("   %s\n", roots[i]);
-      if (roots[i] != exp[i]) begin
-         `uvm_error("ROOTS", $psprintf(" Root does not match \"%s\".", exp[i]));
-      end
-   end
+    function void check_roots(string name,
+            string roots[$],
+            string exp[]);
+        $write("Path(s) to %s:\n", name);
+        foreach (roots[i]) begin
+            $write("   %s\n", roots[i]);
+            if (roots[i] != exp[i]) begin
+                `uvm_error("ROOTS", $psprintf(" Root does not match \"%s\".", exp[i]));
+            end
+        end
    
-endfunction
+    endfunction
 
 
-function void check_paths(string name,
-                          uvm_hdl_path_concat paths[$],
-                          uvm_hdl_path_concat exp[]);
-   $write("Path(s) to %s:\n", name);
-   foreach (paths[i]) begin
-      uvm_hdl_path_concat slices;
-      uvm_hdl_path_concat exp_sl;
+    function void check_paths(string name,
+            uvm_hdl_path_concat paths[$],
+            uvm_hdl_path_concat exp[]);
+        $write("Path(s) to %s:\n", name);
+        foreach (paths[i]) begin
+            uvm_hdl_path_concat slices;
+            uvm_hdl_path_concat exp_sl;
 
-      slices = paths[i];
-      exp_sl = exp[i];
+            slices = paths[i];
+            exp_sl = exp[i];
 
-      $write("   %s\n", uvm_hdl_concat2string(slices));
-      foreach (slices[j]) begin
-         if (slices[i].path != exp_sl[j].path) begin
-            `uvm_error("PATHS", $psprintf(" Path does not match \"%s\".", exp_sl[j].path));
-         end
-      end
-   end
+            $write("   %s\n", uvm_hdl_concat2string(slices));
+            foreach (slices.data[j]) begin
+                if (slices.data[i].path != exp_sl.data[j].path) begin
+                    `uvm_error("PATHS", $psprintf(" Path does not match \"%s\".", exp_sl.data[j].path));
+                end
+            end
+        end
    
-endfunction
+    endfunction
 
 
-initial
-begin
-   uvm_hdl_path_concat paths[$];
-   uvm_hdl_path_slice  slice;
-   string roots[$];
+    initial
+    begin
+        uvm_hdl_path_concat paths[$];
+        uvm_hdl_path_slice slice;
+        string roots[$];
    
-   top_blk model = new("model");
+        top_blk model;
    
-   model.build();
-   model.set_hdl_path_root("$root.dut");
+        model = new("model");
+   
+        model.build();
+        model.set_hdl_path_root("$root.dut");
 
-   model.b1.get_full_hdl_path(roots);
-   check_roots("model.b1", roots, '{"$root.dut.b1"});
+        model.b1.get_full_hdl_path(roots);
+ 
+`ifdef INCA 
+        begin
+            string t_[1]; t_[0]="$root.dut.b1";
+            check_roots("model.b1", roots, t_);
+        end
+`else
+        check_roots("model.b1", roots, '{"$root.dut.b1"});
+`endif 
 
-   model.b1.r1.get_full_hdl_path(paths);
-   check_paths("model.b1.r1", paths,'{ '{ '{"$root.dut.b1.r1", -1, -1} } });
+        model.b1.r1.get_full_hdl_path(paths);
+   
+        begin
+            uvm_hdl_path_concat t_;
+            uvm_hdl_path_concat ta_[1];
+            
+            t_=new();
+   	
+`ifdef INCA 
+            t_.push_back_path("$root.dut.b1.r1", -1, -1);
+            ta_[0]=t_;
+            check_paths("model.b1.r1", paths,ta_);
+`else
+            t_.set('{ '{"$root.dut.b1.r1", -1, -1} });
+            check_paths("model.b1.r1", paths,'{ t_ });
+`endif     	
+        end
 
-   begin
-      uvm_report_server svr;
-      svr = _global_reporter.get_report_server();
+        begin
+            uvm_report_server svr;
+            svr = _global_reporter.get_report_server();
 
-      svr.summarize();
+            svr.summarize();
 
-      if (svr.get_severity_count(UVM_FATAL) +
-          svr.get_severity_count(UVM_NOT_OK) == 0)
-         $write("** UVM TEST PASSED **\n");
-      else
-         $write("!! UVM TEST FAILED !!\n");
-   end
-end
+            if (svr.get_severity_count(UVM_FATAL) +
+                    svr.get_severity_count(UVM_NOT_OK) == 0)
+                $write("** UVM TEST PASSED **\n");
+            else
+                $write("!! UVM TEST FAILED !!\n");
+        end
+    end
 
 endprogram
