d66aef064c4580af58bd58ccd686afe5467629e6
Janick Bergeron <janick@synopsys.com>
2011-01-28 08:35:39 -0800

    Fixed bug in in-order comparator. Documentation of uvm_pair is incorrect but has not been fixed.

---
+++ tests/05components/50in_order/20int/test.sv
@@ -0,0 +1,59 @@
+//---------------------------------------------------------------------- 
+//   Copyright 2011 Synopsys, Inc. 
+//   All Rights Reserved Worldwide 
+// 
+//   Licensed under the Apache License, Version 2.0 (the 
+//   "License"); you may not use this file except in 
+//   compliance with the License.  You may obtain a copy of 
+//   the License at 
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0 
+// 
+//   Unless required by applicable law or agreed to in 
+//   writing, software distributed under the License is 
+//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+//   CONDITIONS OF ANY KIND, either express or implied.  See 
+//   the License for the specific language governing 
+//   permissions and limitations under the License. 
+//----------------------------------------------------------------------
+
+
+program top;
+
+import uvm_pkg::*;
+
+initial
+begin
+   uvm_in_order_built_in_comparator#(int) sb;
+
+   uvm_analysis_port#(int) exp = new("exp", null);
+   uvm_analysis_port#(int) obs = new("obs", null);
+
+   int v[10] = '{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+   fork
+      foreach (v[i]) begin
+         exp.write(v[i]);
+      end
+   join_none
+
+   #10;
+   foreach (v[i]) begin
+      obs.write(v[i]);
+   end
+   
+   begin
+      uvm_report_server svr;
+      svr = _global_reporter.get_report_server();
+
+      svr.summarize();
+
+      if (svr.get_severity_count(UVM_FATAL) +
+          svr.get_severity_count(UVM_ERROR) == 0)
+         $write("** UVM TEST PASSED **\n");
+      else
+         $write("!! UVM TEST FAILED !!\n");
+   end
+end
+
+endprogram
