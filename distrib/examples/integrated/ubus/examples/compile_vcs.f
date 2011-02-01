23734e233af3a55cb4b65fd5d0115b56850c00a8
Janick Bergeron <janick@synopsys.com>
2011-02-01 11:39:38 -0800

    Removed old -f file

--- distrib/examples/integrated/ubus/examples/compile_vcs.f
+++
@@ -1,8 +0,0 @@
-#usage: vcs -file compile_vcs.f 
-
--sverilog -timescale=1ns/1ns
-+incdir+../sv
-+incdir+../../../src
-../../../src/uvm_pkg.sv
-ubus_tb_top.sv
-+acc +vpi ../../../src/dpi/uvm_dpi.cc -cflags -DVCS
