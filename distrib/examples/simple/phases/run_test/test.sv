c76e627eefe8f3762c8a27a0ba27ba2adb45939e
Adam Erickson <adam_erickson@mentor.com>
2011-02-17 08:20:34 -0800

    Fix unresolved merge conflicts in copyright header area

--- distrib/examples/simple/phases/run_test/test.sv
+++ distrib/examples/simple/phases/run_test/test.sv
@@ -2,7 +2,6 @@
 //----------------------------------------------------------------------
 //   Copyright 2007-2010 Mentor Graphics Corporation
 //   Copyright 2007-2011 Cadence Design Systems, Inc.
->>>>>>> c31299a6c1084d71a7123d42d21c1d13527ae376:distrib/examples/simple/phases/run_test/test.sv
 //   All Rights Reserved Worldwide
 //
 //   Licensed under the Apache License, Version 2.0 (the
