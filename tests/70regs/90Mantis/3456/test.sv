a477c5741fc545ef54d83d3dabe50a946cda3be3
Janick Bergeron <janick@synopsys.com>
2011-03-24 12:03:24 -0700

    Testcase for Mantis 3456

---
+++ tests/70regs/90Mantis/3456/test.sv
@@ -0,0 +1,129 @@
+//---------------------------------------------------------------------- 
+//   Copyright 2010-2011 Synopsys, Inc. 
+//   Copyright 2011 Cadence Design Systems, Inc.
+//   All Rights Reserved Worldwide 
+// 
+//   Licensed under the Apache License, Version 2.0 (the 
+//   "License"); you may not use this file except in 
+//   compliance with the License.  You may obtain a copy of 
+//   the License at 
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0 
+// 
+//   Unless required by applicable law or agreed to in 
+//   writing, software distributed under the License is 
+//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+//   CONDITIONS OF ANY KIND, either express or implied.  See 
+//   the License for the specific language governing 
+//   permissions and limitations under the License. 
+//----------------------------------------------------------------------
+
+
+program top;
+
+import uvm_pkg::*;
+
+class reg1 extends uvm_reg;
+   `uvm_object_utils(reg1)
+
+   uvm_reg_field data;
+
+   function new(string name = "reg1");
+      super.new(name,32,UVM_NO_COVERAGE);
+   endfunction
+
+   virtual function void build();
+      data = uvm_reg_field::type_id::create("data",,get_full_name());
+      data.configure(this, 32,  0, "RW", 0,   'h0, 1, 0, 1);
+   endfunction
+endclass
+
+
+class blk1 extends uvm_reg_block;
+   `uvm_object_utils(blk1)
+
+   reg1 r1;
+   reg1 r2;
+   
+   function new(string name = "blk1");
+      super.new(name, UVM_NO_COVERAGE);
+   endfunction
+
+   function void build();
+      default_map = create_map("", 0, 4, UVM_LITTLE_ENDIAN);
+      
+      r1 = reg1::type_id::create("r1",,get_full_name());
+      r1.configure(this, null, "");
+      r1.build();
+
+      r2 = reg1::type_id::create("r2",,get_full_name());
+      r2.configure(this, null, "");
+      r2.build();
+
+      default_map.add_reg(r1, 0);
+      default_map.add_reg(r2, 'h10);
+   endfunction
+endclass
+
+
+class blk2 extends uvm_reg_block;
+   blk1 b1;
+   blk1 b2;
+
+   `uvm_object_utils(blk2)
+
+   function new(string name = "blk2");
+      super.new(name, UVM_NO_COVERAGE);
+   endfunction
+
+   function void build();
+      b1 = blk1::type_id::create("b1");
+      b1.configure(this);
+      b1.build();
+
+      b2 = blk1::type_id::create("b2");
+      b2.configure(this);
+      b2.build();
+
+      default_map = create_map("", 0, 1, UVM_LITTLE_ENDIAN);
+      default_map.add_submap(b1.default_map, 0);
+      default_map.add_submap(b2.default_map, 'h1000);
+   endfunction
+endclass
+
+
+function void check_address(uvm_reg rg, uvm_reg_addr_t exp);
+   $write("Checking address of \"%s\"...\n", rg.get_full_name());
+   if (rg.get_address() !== exp) begin
+      `uvm_error("TEST",
+                 $sformatf("Register \"%s\" is at address 'h%h instead of 'h%h",
+                           rg.get_full_name(), rg.get_address(), exp));
+   end
+endfunction
+
+initial
+begin
+   blk2 blk = blk2::type_id::create("blk");
+   blk.build();
+   blk.lock_model();
+
+   check_address(blk.b1.r1, 'h0000);
+   check_address(blk.b1.r2, 'h0040);
+   check_address(blk.b2.r1, 'h1000);
+   check_address(blk.b2.r2, 'h1040);
+          
+   begin
+      uvm_report_server svr;
+      svr = _global_reporter.get_report_server();
+
+      svr.summarize();
+
+      if (svr.get_severity_count(UVM_FATAL) == 0 &&
+          svr.get_severity_count(UVM_ERROR) == 0)
+         $write("** UVM TEST PASSED **\n");
+      else
+         $write("!! UVM TEST FAILED !!\n");
+   end
+end
+
+endprogram
