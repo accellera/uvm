4a5f042e7cb21bdf2737e6e13f43fd27ccdb2e4f
John Rose <jlrose@cadence.com>
2011-04-12 11:12:30 -0400

    Fixed mantis item 3498 (autoconfig of associative arrays). However, due to the issue in 3497, the set_configs are in the wrong order. When 3497 is fixed, this test and the example will need to be changed.

--- distrib/examples/simple/configuration/automated/top.sv
+++ distrib/examples/simple/configuration/automated/top.sv
@@ -56,13 +56,12 @@ module top;
     //set configuration prior to creating the environment
     set_config_int("topenv.*.u1", "v", 30);
     set_config_int("topenv.inst2.u1", "v", 10);
-    set_config_int("topenv.*", "debug", 1);
     set_config_int("*", "recording_detail", 0);
+    set_config_string("topenv.inst1.u1", "myaa[foo]", "boo");
+    set_config_string("topenv.inst1.u1", "myaa[foobar]", "boobah");
     set_config_string("*", "myaa[foo]", "hi");
     set_config_string("*", "myaa[bar]", "bye");
     set_config_string("*", "myaa[foobar]", "howdy");
-    set_config_string("topenv.inst1.u1", "myaa[foo]", "boo");
-    set_config_string("topenv.inst1.u1", "myaa[foobar]", "boobah");
 
     topenv = new("topenv", null);
 
