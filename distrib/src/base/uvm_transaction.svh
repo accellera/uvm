d86d13f905d9d99b16bb596e9dda92f352500716
Adam Erickson <adam_erickson@mentor.com>
2011-10-18 22:29:05 -0700

    Revert removal of initializers of integer (4-state) to 0.

--- distrib/src/base/uvm_transaction.svh
+++ distrib/src/base/uvm_transaction.svh
@@ -468,8 +468,8 @@ virtual class uvm_transaction extends uvm_object;
   local time    accept_time=-1;
 
   local uvm_component initiator;
-  local integer       stream_handle;
-  local integer       tr_handle;
+  local integer       stream_handle=0;
+  local integer       tr_handle=0;
   local bit           record_enable;
   local uvm_recorder  m_recorder;
 
