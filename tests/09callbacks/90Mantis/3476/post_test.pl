696ce2b09e0ee8e0925e2bfb250baa1c88939bc4
Uwe Simm <uwes@cadence.com>
2011-10-20 11:26:45 +0200

    updated post filter and log file to ignore handles/etc

--- tests/09callbacks/90Mantis/3476/post_test.pl
+++ tests/09callbacks/90Mantis/3476/post_test.pl
@@ -17,70 +17,42 @@
 ##   permissions and limitations under the License. 
 ##----------------------------------------------------------------------
 
-
-#
-# Available variables:
-#
-# $log        Path and name of run-time log file
-# $testdir    Path to test directory
-#
-# Return:
-#
-# $post_test  Reason for success (or failure)
-# 1           If test passes
-# 0           If test fails
-
-# This is a gold file test. The test prints "START OF GOLD FILE"
-# at the start of comparison output and "END OF GOLD FILE TEST"
-# at the end. Leading hashes (#) on lines are ignored to accomodate
-# the output style of the Questa transcript.
-#
-# The log file is parsed and the data between the START/END markers
-# are saved in an output file called output. This output file
-# is then diffed with the gold file, output.au. A value of 1 is
-# returned if there is no diff and 0 is returned on a diff. If
-# a diff exists it will be output in a file called output.df.
-
-if (! -e $log) {
-   $post_test = "No logfile";
-   return 1;
+if (!open(LOG, "<$log")) {
+  $post_test = "Cannot open \"$log\" for reading: $!";
+  return 1;
 }
 
-$path = `dirname $log`; chomp $path;
+$logfile=qx{cat "$log"};
+close(LOG);
 
-$gold = "$path/output.au";
-$newlog = "$path/output";
+$logfile =~ s/\@\d+/\@X/sg;
+# strip header
+$logfile =~ s/.*\nSTART\s+OF\s+GOLD\s+FILE\n//sx;
+$logfile =~ s/\nEND\s+OF\s+GOLD\s+FILE\n.*//sx;
+$logfile =~ s/^ncsim>.*$//mg;
+$logfile =~ s/^.*\.svh.*$//mg;
+$logfile =~ s/\n+\n/\n/sxg;
+$logfile =~ s/^UVM-\S+\s+\(\S+\)$/UVM-VERSION/mg;
+$logfile =~ s/^\(C\).*$/COPYRIGHT/mg;
+$logfile =~ s/COPYRIGHT(.COPYRIGHT)+/COPYRIGHT/sg;
+$logfile =~ s/^SVSEED.*\n//sg;
+$logfile =~ s/^# //mg;
 
-open NEWLOG,">  $newlog"  or return 0;
-open LOG, "<  $log"  or return 0;
 
-$log_line = "";
-$is_started = 0;
-while($log_line = <LOG>) {
-  if(!$is_started) {
-    if($log_line =~ /START OF GOLD FILE/) { $is_started = 1; }
-  } else {
-    if($log_line =~ /END OF GOLD FILE/) { $is_started = 0; }
-    else {
-      # remove '# ' from logs (Questa)
-      $log_line =~ s/^# //;
-      print NEWLOG "$log_line";
-    }
-  }
+
+# write back
+
+if (!open(L, ">$testdir/post.log")) {
+  $post_test = "Cannot open \"$testdir/post.log\" for writing: $!";
+  close(L);
+  return 1;
+}
+print L $logfile;
+close(L);
+
+if (system("diff $testdir/post.log $testdir/log.au > $testdir/log.df")) {
+  $post_test = "$log and log.au differ";
+  return 1;
 }
 
-close NEWLOG;
-close LOG;
-
-system("diff $gold $newlog > $path/output.df");
-if($? == 0) {
-  $post_test = "gold file matched";
-  system("rm -f $path/output.df");
-  system("rm -f $newlog") unless $opt_d;
-  return 0;
-}
-
-$post_test = "gold file mismatched";
-system("rm -f $newlog") unless $opt_d;
-return 1;
-
+return 0;
