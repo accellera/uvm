c2657c170bff63399ad101140986013ce97966fc
Janick Bergeron <janick@synopsys.com>
2011-11-30 09:23:40 -0800

    Updated 1.1b to latest 1.1a Updated version number to 1.1b-DEV

--- distrib/src/macros/uvm_version_defines.svh
+++ distrib/src/macros/uvm_version_defines.svh
@@ -27,7 +27,7 @@
 `define UVM_NAME UVM
 `define UVM_MAJOR_REV 1
 `define UVM_MINOR_REV 1
-`define UVM_FIX_REV a
+`define UVM_FIX_REV b-DEV
 
 
 // Whole version identifiers that can be used in `ifdefs and `ifndefs
