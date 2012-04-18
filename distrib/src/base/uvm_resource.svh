21352d129337ab11f56104a024e6798e5a6ebc03
Mark Glassear <mxg@cypress.com>
2012-04-18 10:09:28 -0700

    Properly fixed Mantis 3912

--- distrib/src/base/uvm_resource.svh
+++ distrib/src/base/uvm_resource.svh
@@ -495,14 +495,21 @@ virtual class uvm_resource_base extends uvm_object;
     if(!uvm_resource_options::is_auditing())
       return;
 
-    if(accessor != null) begin
+    // If an accessor is supplied, then use its name
+	// as the database entry for the accessor record.
+	// Otherwise, use "<empty>" as the database entry.
+    if(accessor != null)
       str = accessor.get_full_name();
-      if(access.exists(str))
-        access_record = access[str];
-      else
-        init_access_record(access_record);
-    end
+    else
+      str = "<empty>";
 
+    // Create a new accessor record if one does not exist
+    if(access.exists(str))
+      access_record = access[str];
+    else
+      init_access_record(access_record);
+
+    // Update the accessor record
     access_record.read_count++;
     access_record.read_time = $realtime;
     access[str] = access_record;
