affb950ef5f5118b1d0318fb6503e4c3eaaeedca
Justin Refice <Justin.Refice@amd.com>
2012-07-26 15:04:18 -0400

    Fixing the test to actually catch the item

--- tests/20sequences/99Mantis_fixes/4194_children_sequence_die/test.sv
+++ tests/20sequences/99Mantis_fixes/4194_children_sequence_die/test.sv
@@ -22,7 +22,9 @@ class child_sequence extends uvm_sequence#(simple_item);
    endfunction : new
 
    task body();
+      `uvm_info("CHILD_BODY", "before do", UVM_LOW)
       `uvm_do(req)
+      `uvm_info("CHILD_BODY", "after do", UVM_LOW)
    endtask : body
 
    function void do_kill();
@@ -33,17 +35,20 @@ class child_sequence extends uvm_sequence#(simple_item);
 
 endclass : child_sequence
    
-class parent_sequence extends uvm_sequence#(simple_item);
+class parent_sequence extends uvm_sequence;
    `uvm_object_utils(parent_sequence)
+
+   uvm_sequencer#(simple_item) real_sequencer;
+   child_sequence c;
 
    function new(string name="unnamed-parent_sequence");
       super.new(name);
    endfunction : new
 
    task body();
-      child_sequence c = child_sequence::type_id::create("c");
+      c = child_sequence::type_id::create("c");
 
-      c.start(get_sequencer(), this);
+      c.start(real_sequencer, this);
    endtask : body
      
    function void do_kill();
@@ -65,8 +70,9 @@ class simple_driver extends uvm_driver#(simple_item);
       simple_item item;
       forever begin
          #10;
+         `uvm_info("DRIVER_GET", "fetching item", UVM_LOW)
          seq_item_port.get(item);
-         `uvm_info("ITEM", "recieved item", UVM_LOW)
+         `uvm_info("DRIVER_GOT", "recieved item", UVM_LOW)
       end
    endtask : run_phase
 
@@ -78,6 +84,7 @@ class test extends uvm_test;
    parent_sequence p;
    simple_driver driver;
    uvm_sequencer#(simple_item) sequencer;
+   uvm_sequencer virtual_sequencer;
 
    function new(string name="unnamed-test", uvm_component parent=null);
       super.new(name, parent);
@@ -88,6 +95,8 @@ class test extends uvm_test;
 
       driver = new("driver");
       sequencer = new("sequencer");
+      virtual_sequencer = new("virtual_sequencer");
+      
    endfunction : build_phase
 
    function void connect_phase(uvm_phase phase);
@@ -99,18 +108,25 @@ class test extends uvm_test;
       phase.raise_objection(this);
 
       p = parent_sequence::type_id::create("p1");
+      p.real_sequencer = sequencer;
       fork : first_fork
-         p.start(sequencer);
+         p.start(virtual_sequencer);
       join_none : first_fork
       #1;
       p.kill();
 
+      begin
+         uvm_sequence_state_enum m_state = p.c.get_sequence_state();
+         `uvm_info("TEST", $sformatf("after first kill, p1.c.state = '%s'", m_state.name()), UVM_LOW)
+      end
+
       p = parent_sequence::type_id::create("p2");
+      p.real_sequencer = sequencer;
       fork : second_fork
-         p.start(sequencer);
+         p.start(virtual_sequencer);
       join_none : second_fork
 
-      #10;
+      #15;
       phase.drop_objection(this);
    endtask : run_phase
 
