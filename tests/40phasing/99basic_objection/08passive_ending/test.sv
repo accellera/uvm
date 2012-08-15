c2a4a1bb17a0ca688463e6ff482365a80d9c1e6c
Justin Refice <Justin.Refice@amd.com>
2012-08-15 16:49:24 -0400

    beefing up the phase controller a bit

--- tests/40phasing/99basic_objection/08passive_ending/test.sv
+++ tests/40phasing/99basic_objection/08passive_ending/test.sv
@@ -5,6 +5,28 @@ module test;
 
    // include the experimental "phase objection" (contains protections and whatnot)
 `include "uvm_phase_objection.svh"
+
+class prolonger_sequence extends uvm_sequence;
+   uvm_phase_objection phase_objection;
+
+   `uvm_object_utils(prolonger_sequence)
+
+   function new(string name="unnamed-prolonger_sequence");
+      super.new(name);
+   endfunction : new
+
+   task body();
+      `uvm_info("PROLONGER_SEQ/BODY", "inside prolonger body", UVM_NONE)
+      phase_objection.wait_for_raise_requested_count(0, UVM_GT);
+      `uvm_info("PROLONGER_SEQ/BODY", "saw request to start phase", UVM_NONE)
+      phase_objection.raise_objection(this, "prolonging");
+      #5;
+      `uvm_info("PROLONGER_SEQ/BODY", "dropping our request to prolong the phase", UVM_NONE)
+      phase_objection.drop_objection(this, "done prolonging");
+   endtask : body
+
+endclass : prolonger_sequence
+   
    
 class wait_sequence extends uvm_sequence;
 
@@ -88,6 +110,7 @@ class test extends uvm_test;
 
    wait_sequence ws;
    cb_sequence  cs;
+   prolonger_sequence ps;
    uvm_sequencer sqr;
    uvm_phase_objection our_phase;
 
@@ -112,32 +135,52 @@ class test extends uvm_test;
       ws.ending_phase_objection = our_phase;
       cs = new("cs");
       cs.ending_phase_objection = our_phase;
+      ps = new("ps");
+      ps.phase_objection = our_phase;
 
       fork 
          ws.start(sqr);
          cs.start(sqr);
+         ps.start(sqr);
       join_none
 
-      `uvm_info("TEST", "sending request to start our_phase", UVM_NONE)
-      our_phase.request_to_raise(this, "request to start the phase");
-      `uvm_info("TEST", "prolonging our_phase...", UVM_NONE)
-      our_phase.raise_objection(this, "prolonging");
-      #5;
-      `uvm_info("TEST", "letting the phase end...", UVM_NONE)
-      our_phase.drop_objection(this, "done prolonging");
       #1;
-
+      
+      `uvm_info("TEST", "sending request to raise our_phase (phase starting)", UVM_NONE)
+      our_phase.request_to_raise(this, "request to start the phase");
+      uvm_wait_for_nba_region();
+      if (our_phase.get_sum() > 0) begin
+        `uvm_info("TEST", "saw requests to prolong phase, raising our own objection", UVM_NONE)
+        our_phase.raise_objection(this, "phase is really happening");
+        do 
+          begin
+           `uvm_info("TEST", "waiting for everyone to drop their objections", UVM_NONE)
+           our_phase.wait_for_sum(1);
+           `uvm_info("TEST", "calling request to raise our_phase (phase ready_to_end)", UVM_NONE)
+           uvm_wait_for_nba_region();
+          end
+         while (our_phase.get_sum() > 1);
+         `uvm_info("TEST", "our_phase is done, dropping our objection", UVM_NONE)
+         our_phase.drop_objection(this, "phase is done");
+      end
+      else begin
+         `uvm_info("TEST", "no one responded to our request, skipping the phase", UVM_NONE)
+         // Clear the objection to get rid of our request_to_raise
+         our_phase.clear(this, "skipping phase");
+      end // else: !if(our_phase.get_sum() > 0)
+      
       phase.drop_objection(this);
    endtask : run_phase
 
    function void report();
-      uvm_sequence_state_enum ws_state, cs_state;
+      uvm_sequence_state_enum ws_state, cs_state, ps_state;
       ws_state = ws.get_sequence_state();
       cs_state = cs.get_sequence_state();
-      if ((ws_state == FINISHED) && (cs_state == FINISHED))
+      ps_state = ps.get_sequence_state();
+      if ((ws_state == FINISHED) && (cs_state == FINISHED) && (ps_state == FINISHED))
         $display("*** UVM TEST PASSED ***");
       else
-        $display("*** UVM TEST FAILED *** - ws.state = '%s', cs.state = '%s'", ws_state.name(), cs_state.name());
+        $display("*** UVM TEST FAILED *** - ws.state = '%s', cs.state = '%s', ps.state = '%s'", ws_state.name(), cs_state.name(), ps_state.name());
    endfunction : report
                               
 endclass : test   
