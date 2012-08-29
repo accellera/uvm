540edc50d5bd1c7852e27737e77b0ce20f5d5c5f
Adam Erickson <adam_erickson@mentor.com>
2012-08-29 08:36:52 -0700

    Mantis 4303 - TLM GP should not pack extensions; fix unpack byte_enable; fix GP do_print/do_record for extensions

--- tests/11tlm/20tlm2/10gp/20extfunc/test.sv
+++ tests/11tlm/20tlm2/10gp/20extfunc/test.sv
@@ -50,6 +50,24 @@ class ext1 extends uvm_tlm_extension#(ext1_ext);
 
 endclass
 
+typedef class ext2;
+typedef ext2 ext2_ext;
+
+class ext2 extends uvm_tlm_extension#(ext2_ext);
+ rand byte b;
+ rand byte c;
+   `uvm_object_utils_begin(ext2)      
+     `uvm_field_int(b, UVM_DEFAULT)
+     `uvm_field_int(c, UVM_DEFAULT)
+   `uvm_object_utils_end
+
+
+  function new(string name="ext2");
+     super.new(name);
+  endfunction
+
+endclass
+
 `else
 
 class ext1 extends uvm_tlm_extension#(ext1);
@@ -60,6 +78,21 @@ class ext1 extends uvm_tlm_extension#(ext1);
 
 
   function new(string name="ext1");
+     super.new(name);
+  endfunction
+
+endclass
+
+class ext2 extends uvm_tlm_extension#(ext2);
+ rand byte b;
+ rand byte c;
+   `uvm_object_utils_begin(ext2)      
+     `uvm_field_int(b, UVM_DEFAULT)
+     `uvm_field_int(c, UVM_DEFAULT)
+   `uvm_object_utils_end
+
+
+  function new(string name="ext2");
      super.new(name);
   endfunction
 
@@ -125,20 +158,40 @@ class test extends uvm_test;
         end
     endfunction
 
+   // compare without considering extensions
+   function bit compare_base_gp (uvm_tlm_gp gp1, uvm_tlm_gp gp2);
+      if ( !(gp1.m_address == gp2.m_address &&
+             gp1.m_command == gp2.m_command &&
+             gp1.m_length == gp2.m_length &&
+             gp1.m_byte_enable_length == gp2.m_byte_enable_length &&
+             gp1.m_streaming_width == gp2.m_streaming_width) )
+        return 0;
+      for (int i=0; i< gp1.m_length; i++)
+        if (gp1.m_data[i] != gp2.m_data[i])
+          return 0;
+      for (int i=0; i< gp1.m_byte_enable_length; i++)
+        if (gp1.m_byte_enable[i] != gp2.m_byte_enable[i])
+          return 0;
+      return 1;
+   endfunction
+
    task run_phase(uvm_phase phase);
       uvm_tlm_gp  obj1=new,obj2=new;
+      uvm_tlm_gp  save_gp;
+      uvm_object obj;
       bit bits[];
 
-      ext1 x1 = new("ext1_inst");
-      ext1 x2 = new("ext2_inst");
+      ext1 x1 = new("ext1_inst1");
+      ext1 x2 = new("ext1_inst2");
+      ext2 x3 = new("ext2_inst1");
      
       phase.raise_objection(this);
 
       assert(x1.randomize());
       assert(x2.randomize() with {a != x1.a;});
+      assert(x3.randomize());
    
       void'(obj1.set_extension(x1));
-      void'(obj2.set_extension(x2));
 
     uvm_default_packer.use_metadata = 1;
     uvm_default_packer.big_endian = 0;
@@ -153,7 +206,7 @@ class test extends uvm_test;
 
        $display("*** TRANS %0d ***",i);
 
-     assert( obj1.randomize() with { 
+       assert( obj1.randomize() with { 
           m_address >= 0 && m_address < 256; 
           m_length == `NUM_TRANS; 
           m_data.size == m_length;
@@ -165,7 +218,7 @@ class test extends uvm_test;
           m_streaming_width == m_length; 
 				     } );
 
-   assert( obj2.randomize() with { 
+       assert( obj2.randomize() with { 
           m_address != obj1.m_address; 
           m_length == obj1.m_length-1; //ensure different sizes
           m_data.size == m_length;
@@ -177,8 +230,9 @@ class test extends uvm_test;
           m_streaming_width == m_length; 
 				     } );
 
+       obj = obj2.clone();
+       $cast(save_gp,obj2.clone());
 
-    
      //---------------------------------
      // COPY
      //---------------------------------
@@ -212,6 +266,7 @@ class test extends uvm_test;
      begin
      string s1,s2;
 
+
      s1 = obj1.sprint(uvm_default_table_printer);
      s2 = obj2.sprint(uvm_default_table_printer);
      filter(s1,s2); 
@@ -236,13 +291,25 @@ class test extends uvm_test;
      // PACK/UNPACK
      //---------------------------------
 
+     // reset obj2, add extension
+     obj2 = save_gp;
+     void'(obj2.set_extension(x2));
+     void'(obj2.set_extension(x3));
+
      void'(obj1.pack(bits));
      void'(obj2.unpack(bits));
+
+     if (!compare_base_gp(obj1, obj2))
      if (!obj1.compare(obj2)) begin
-                  `uvm_error("TEST", "MISCOMPARE");
+                  `uvm_error("TEST", "obj2's base properties should be equal to obj1's");
        obj1.print();
        obj2.print();
      end
+
+     // confirm obj2's extension is untouched
+     if (obj2.get_extension(ext1::ID()) != x2)
+     //if (obj2.get_extension(ext2.get_type_handle()) != ext2)
+                  `uvm_error("TEST", "obj2's extensions should be untouched");
     
     
      //---------------------------------
@@ -256,6 +323,57 @@ class test extends uvm_test;
 
 
    end // for (..NUM_TRANS..)
+
+     //---------------------------------
+     // PRINT - take 2
+     //---------------------------------
+
+     // test output of sprint(table); need to make data deterministic (i.e. assign constants)
+     begin
+       string exp;
+       string act;
+       byte unsigned data[];
+       uvm_default_printer.knobs.reference = 0;
+       data = new[4];
+       data = '{'h11,'h22,'h33,'h44};
+       obj2.set_data(data);
+       obj2.set_byte_enable_length(0);
+       obj2.set_data_length(4);
+       obj2.set_address('h1234567890abcdef);
+       obj2.set_command(UVM_TLM_WRITE_COMMAND);
+       obj2.set_response_status(UVM_TLM_OK_RESPONSE);
+       obj2.set_streaming_width('h87654321);
+       x2.a = 'hff;
+       x3.b = 'h55;
+       x3.c = 'haa;
+       obj2.print();
+       exp = {exp, "--------------------------------------------------------------------------------------\n"};
+       exp = {exp, "Name               Type                       Size  Value                             \n"};
+       exp = {exp, "--------------------------------------------------------------------------------------\n"};
+       exp = {exp, "<unnamed>          uvm_tlm_generic_payload    -     -                                 \n"};
+       exp = {exp, "  address          integral                   64    'h1234567890abcdef                \n"};
+       exp = {exp, "  command          uvm_tlm_command_e          32    UVM_TLM_WRITE_COMMAND             \n"};
+       exp = {exp, "  response_status  uvm_tlm_response_status_e  32    UVM_TLM_OK_RESPONSE               \n"};
+       exp = {exp, "  streaming_width  integral                   32    'h87654321                        \n"};
+       exp = {exp, "  data             darray(byte)               4     -                                 \n"};
+       exp = {exp, "    [0]            byte                       8     'h11                              \n"};
+       exp = {exp, "    [1]            byte                       8     'h22                              \n"};
+       exp = {exp, "    [2]            byte                       8     'h33                              \n"};
+       exp = {exp, "    [3]            byte                       8     'h44                              \n"};
+       exp = {exp, "  extensions       aa(obj,obj)                2     -                                 \n"};
+       exp = {exp, "    [ext1_inst2]   ext1                       -     -                                 \n"};
+       exp = {exp, "      a            integral                   8     'hff                              \n"};
+       exp = {exp, "    [ext2_inst1]   ext2                       -     -                                 \n"};
+       exp = {exp, "      b            integral                   8     'h55                              \n"};
+       exp = {exp, "      c            integral                   8     'haa                              \n"};
+       exp = {exp, "--------------------------------------------------------------------------------------\n"};
+       act = obj2.sprint(uvm_default_table_printer);
+       if (act != exp)  begin
+          `uvm_error("TEST", "obj2's print output is unexpected");
+          $display("\nEXPECT:\n%s\n\n",exp);
+          $display("\nACTUAL:\n%s\n\n",act);
+        end
+     end
 
   phase.drop_objection(this);
 
