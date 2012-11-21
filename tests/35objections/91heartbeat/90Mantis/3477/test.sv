b198f0368434921df04c254915c9d75bcb884277
Adiel Khan <adiel@synopsys.com>
2012-11-21 15:17:22 -0800

    Added copyright to testcase 3477

--- tests/35objections/91heartbeat/90Mantis/3477/test.sv
+++ tests/35objections/91heartbeat/90Mantis/3477/test.sv
@@ -1,6 +1,26 @@
-module top;
-    import uvm_pkg::*;
+//---------------------------------------------------------------------- 
+//   Copyright 2012 Synopsys, Inc. 
+//   All Rights Reserved Worldwide 
+// 
+//   Licensed under the Apache License, Version 2.0 (the 
+//   "License"); you may not use this file except in 
+//   compliance with the License.  You may obtain a copy of 
+//   the License at 
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0 
+// 
+//   Unless required by applicable law or agreed to in 
+//   writing, software distributed under the License is 
+//   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+//   CONDITIONS OF ANY KIND, either express or implied.  See 
+//   the License for the specific language governing 
+//   permissions and limitations under the License. 
+//----------------------------------------------------------------------
+
     `include "uvm_macros.svh"
+program top;
+    import uvm_pkg::*;
+
 
     class test extends uvm_test;
         `uvm_component_utils(test)
@@ -31,11 +51,9 @@ module top;
             phase.drop_objection(this);
         endtask
         function void report_phase(uvm_phase phase);
-            super.report_phase(phase);
             $write("** UVM TEST PASSED **\n");
         endfunction
     endclass
   
     initial run_test();
-endmodule
-
+endprogram
