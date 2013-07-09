b59c7a69f26839ca19c158dd587f110ba7fa7259
Justin Refice <jrefice@nvidia.com>
2013-07-09 14:07:09 -0700

    Removing wrong copyright

--- distrib/src/base/uvm_phase.svh
+++ distrib/src/base/uvm_phase.svh
@@ -3,7 +3,6 @@
 //   Copyright 2007-2011 Mentor Graphics Corporation
 //   Copyright 2007-2010 Cadence Design Systems, Inc.
 //   Copyright 2010-2013 Synopsys, Inc.
-//   Copyright 2013      NVIDIA Corporation
 //   All Rights Reserved Worldwide
 //
 //   Licensed under the Apache License, Version 2.0 (the
