70040ae8db64788afc9aabab160b5ca9656934d5
Justin Refice <jrefice@nvidia.com>
2013-07-09 12:22:05 -0700

    Adding get_objection_count to uvm_phase

--- distrib/src/base/uvm_phase.svh
+++ distrib/src/base/uvm_phase.svh
@@ -352,6 +352,12 @@ class uvm_phase extends uvm_object;
                                                int count=1);
 
 
+  // Function: get_objection_count
+  //
+  // Returns the current number of objections to ending this phase raised by the given ~object~.
+  //
+  extern virtual function int get_objection_count( uvm_object obj=null );
+   
   // Functions: sync and unsync
   //
   // Add soft sync relationships between nodes
@@ -1695,6 +1701,12 @@ function void uvm_phase::drop_objection (uvm_object obj,
   phase_done.drop_objection(obj,description,count);
 endfunction
 
+// get_objection_count
+// -------------------
+
+function void uvm_phase::get_objection_count (uvm_object obj);
+   return phase_done.get_objection_count(obj);
+endfunction : get_objection_count
 
 // sync
 // ----
