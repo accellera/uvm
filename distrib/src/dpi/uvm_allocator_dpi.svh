2e3fc6825e47e2917b41a38536abc12feabe22e3
r56614 <allag@freescale.com>
2013-09-10 17:04:08 +0300

    add allocator dpi functions for UVM_ALLOC_NO_DPI define

--- distrib/src/dpi/uvm_allocator_dpi.svh
+++ distrib/src/dpi/uvm_allocator_dpi.svh
@@ -13,14 +13,32 @@ function void svdpi_get_taken_list(string name, int size, inout longint db[]);
   uvm_report_fatal("SVDPI", 
                    "uvm_item_allocator DPI routines are compiled off. Recompile without +define+UVM_ALLOC_NO_DPI");
 endfunction
+
 function void svdpi_set_taken_list(string name, int size, longint db[]);
   uvm_report_fatal("SVDPI", 
                    "uvm_item_allocator DPI routines are compiled off. Recompile without +define+UVM_ALLOC_NO_DPI");
 endfunction
+
 function int  svdpi_get_num_taken(string name);
   uvm_report_fatal("SVDPI", 
                    "uvm_item_allocator DPI routines are compiled off. Recompile without +define+UVM_ALLOC_NO_DPI");
   return 0;
+endfunction 
+  
+task svdpi_lock_taken_list(string name);
+  uvm_report_fatal("SVDPI", 
+                   "uvm_item_allocator DPI routines are compiled off. Recompile without +define+UVM_ALLOC_NO_DPI");
+endtask 
+  
+function bit svdpi_try_lock_taken_list(string name);
+  uvm_report_fatal("SVDPI", 
+                   "uvm_item_allocator DPI routines are compiled off. Recompile without +define+UVM_ALLOC_NO_DPI");
+  return 0;
+endfunction 
+  
+function void svdpi_unlock_taken_list(string name);
+  uvm_report_fatal("SVDPI", 
+                   "uvm_item_allocator DPI routines are compiled off. Recompile without +define+UVM_ALLOC_NO_DPI");
 endfunction
 `endif
 
