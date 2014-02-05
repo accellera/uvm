d4303677cd5258edc9d059cec82a89667cfe45c8
Justin Refice <jrefice@nvidia.com>
2014-02-05 11:00:00 -0800

    Missed copyright

--- distrib/src/macros/uvm_version_defines.svh
+++ distrib/src/macros/uvm_version_defines.svh
@@ -2,6 +2,7 @@
 //   Copyright 2007-2010 Mentor Graphics Corporation
 //   Copyright 2007-2010 Cadence Design Systems, Inc. 
 //   Copyright 2010-2011 Synopsys, Inc.
+//   Copyright 2014      NVIDIA Corporation
 //   All Rights Reserved Worldwide
 //
 //   Licensed under the Apache License, Version 2.0 (the
